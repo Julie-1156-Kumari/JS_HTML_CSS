7ef17ccb4a211c101efe1511efe2a980cc1ca5f7
Julie Kumari <chawla1julie@gmail.com>
2024-12-04 21:37:01 +0100

    playingwithcard director uploaded

---
+++ Playing_with_colors/playingWithColors.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll(".card");
+for(let i=0; i<cards.length; i++){
+    const currColor = cards[i];
+    currColor.addEventListener("dblclick", () => {
+        const color = currColor.getAttribute("data-color");
+
+        if(color != "used"){
+            currColor.classList.add(color);
+            currColor.setAttribute("data-used", "used");
+        }
+    });
+}
