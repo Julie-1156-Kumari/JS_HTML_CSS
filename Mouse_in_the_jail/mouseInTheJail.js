c0dcee03af42cba745e5a54ae343f4c58a9f89f9
Julie Kumari <chawla1julie@gmail.com>
2024-12-06 11:46:18 +0100

    updated in Mouse_in_the_jail

--- Mouse_in_the_jail/mouseInTheJail.js
+++ Mouse_in_the_jail/mouseInTheJail.js
@@ -1,9 +1,20 @@
+// Select the "real mouse" element
 const jail = document.querySelector(".cell");
+const mousePointer = document.querySelector('#mouse-pointer');
+
+
+
 jail.addEventListener("mouseover", () => {
     jail.classList.remove("out-box");
     jail.classList.add("in-box");
 });
-cell.addEventListener("mouseleave", () => {
+jail.addEventListener("mouseleave", () => {
     jail.classList.remove("in-box");
     jail.classList.add("out-box");
-  });
+});
+// Update its position on mouse move
+document.addEventListener('mousemove', (event) => {
+    const { clientX, clientY } = event; // Get cursor coordinates
+    mousePointer.style.left = `${clientX}px`;
+    mousePointer.style.top = `${clientY}px`;
+});
